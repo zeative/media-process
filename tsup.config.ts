eec36abbb67fc88b51f4e708fbb378292f3f96fa
zaadevofc <zaadevofc@gmail.com>
2026-03-17 08:20:01 +0700

    chore: update package name and extensively configure tsup for optimized builds

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,23 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
+  outDir: 'dist',
+
   dts: true,
+  splitting: false,
   clean: true,
-  target: 'es2022',
+  minify: true,
+  sourcemap: false,
+  treeshake: true,
+  legacyOutput: false,
+  shims: true,
+
+  outExtension({ format }) {
+    return {
+      js: format === 'cjs' ? '.js' : '.mjs',
+    };
+  },
+
+  // tsconfig: './tsconfig.json',
+  noExternal: []
 });
